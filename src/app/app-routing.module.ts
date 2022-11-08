c45948e8126c3cea454051c86cba07cda7af6cd1
Jeeva <jeevabaskaran328@gmail.com>
2022-11-08 14:51:09 +0530

    Routing & navigation , Wildcard & redirecting rout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path :'',component:AppComponent,pathMatch:'full'}
+  ,{path:'employeeDetails' , component : EmployeeDetailComponent},
+{path:'employeeList' , component : EmployeeListComponent},
+{path:"**",component:PageNotFoundComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [EmployeeDetailComponent,EmployeeListComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule,routingComponents} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { EmployeeService } from './employee.service';
-import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
-import { EmployeeListComponent } from './employee-list/employee-list.component';
-import {HttpClientModule} from '@angular/common/http'
+import {HttpClientModule} from '@angular/common/http';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
-    EmployeeListComponent,
-    EmployeeDetailComponent
+    routingComponents,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [EmployeeService],
   bootstrap: [AppComponent]
