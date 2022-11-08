3dc8222b4b470f9163deb0d5165eb90fe45d9eb9
Jeeva <jeevabaskaran328@gmail.com>
2022-11-08 11:30:29 +0530

    http error handling

--- src/app/employee-detail/employee-detail.component.ts
+++ src/app/employee-detail/employee-detail.component.ts
@@ -9,13 +9,15 @@ import { EmployeeService } from '../employee.service';
 export class EmployeeDetailComponent implements OnInit {
 
   public employees:any = []
+  public errMessage:any
 
   constructor(private _employeeService : EmployeeService) { }
 
   ngOnInit(): void {
     // this.employees = this._employeeService.getEmployees()
     this._employeeService.getEmployees()
-    .subscribe(data => this.employees=data)
+    .subscribe(data => this.employees=data,
+      error => this.errMessage = error)
   }
 
 }
--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -4,9 +4,10 @@
  * 2) Register with injector (if we don't register the service will be treated as another regular class)
  * 3) Declare as dependency in components
  */
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable ,throwError} from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -20,7 +21,11 @@ export class EmployeeService {
   //   {"id":2 , "name" : "Naveen" , "age" : 23},
   //   {"id":3 , "name" : "Vignesh" , "age" : 22},
   //   {"id":4 , "name" : "Guna" , "age" : 20}]
-  return this.http.get<[]>("https://reqres.in/api/users?page=2")
+
+  return this.http.get<[]>("https://reqres.in/api/users?page=2").pipe(catchError(this.errorHandler))
   }
 
+  errorHandler(error: HttpErrorResponse){
+    return throwError(()=> new Error(error.message || "Server error"))
+  }
 }
